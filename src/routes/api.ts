2192424bb7555cdda2a1d0b0fdfcb9423d5adb07
Samuel Mameru Carr <samuelcarr.official@gmail.com>
2024-03-01 17:47:01 +0530

    Sam | Added pingDB()

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,10 +1,17 @@
 import express, {Request, Response, Router} from "express";
+import { pingDB } from "../services/database/database.js";
 
 const apiRouter = Router();
 
 apiRouter.use(express.json());
 
-apiRouter.get("/check", (request: Request, response: Response): void => {
+apiRouter.get("/check", async (request: Request, response: Response): Promise<void> => {
+    try {
+        await pingDB();
+    }
+    catch (error) {
+        response.json({"message": "Database Error", "error": error});
+    }
     response.json({"message": "API is up and running!"});
 });
 
