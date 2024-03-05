020891ef25c7867a608a78be2b9f74b3a25bac2e
Samuel Mameru Carr <samuelcarr.official@gmail.com>
2024-03-05 14:10:29 +0530

    Sam | Added getService Controller to route

--- src/routes/serviceMaster/serviceMaster.ts
+++ src/routes/serviceMaster/serviceMaster.ts
@@ -1,9 +1,10 @@
 
 import { Router, Response, Request } from "express";
-import { postService } from "../../controllers/serviceMaster/serviceMaster.js";
+import {getService, postService} from "../../controllers/serviceMaster/serviceMaster.js";
 
 const serviceMasterRouter: Router = Router();
 
 serviceMasterRouter.post("/", postService);
+serviceMasterRouter.get("/", getService);
 
 export default serviceMasterRouter;
