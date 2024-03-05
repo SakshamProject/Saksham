2534387c8d4066bd4942ec2c4147a5cfd86682dc
Samuel Mameru Carr <samuelcarr.official@gmail.com>
2024-03-05 15:06:22 +0530

    Sam | Zod Parse request.query

--- src/controllers/serviceMaster/serviceMaster.ts
+++ src/controllers/serviceMaster/serviceMaster.ts
@@ -1,11 +1,21 @@
 import { Request, Response } from "express";
+import {ZodError} from "zod";
+import getRequestSchema from "../getRequest.schema.js";
 
 async function postService(request: Request, response: Response): Promise<void> {
     response.json(request.body);
 }
 
 async function getService(request: Request, response: Response): Promise<void> {
-
+    try {
+        const query = getRequestSchema.parse(request.query);
+        response.json(query);
+    }
+    catch (error) {
+        if (error instanceof ZodError) {
+            response.json(error);
+        }
+    }
 }
 
 export { postService, getService };
