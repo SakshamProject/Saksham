be7fb768ed3349b8b4c1666f198bf914e8d385e5
Samuel Mameru Carr <samuelcarr.official@gmail.com>
2024-03-07 14:56:52 +0530

    Sam | Wrap try-catch and parse using schema

--- src/controllers/serviceMaster/serviceMaster.ts
+++ src/controllers/serviceMaster/serviceMaster.ts
@@ -1,10 +1,19 @@
 import { Request, Response } from "express";
 import {ZodError} from "zod";
 import {getRequestSchema} from "../schemas/zodSchemas.js";
+import postServiceMasterSchema from "./serviceMaster.schema.js";
 
 async function postService(request: Request, response: Response): Promise<void> {
+    try {
+        const body = postServiceMasterSchema.parse(request.body);
 
-    response.json(request.body);
+        response.json(body);
+    }
+    catch (error) {
+        if (error instanceof ZodError) {
+            response.json(error);
+        }
+    }
 }
 
 async function getService(request: Request, response: Response): Promise<void> {
