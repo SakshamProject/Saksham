85337de93488f5bc7fa6ff9f3701a1d29cf1166e
Samuel Mameru Carr <samuelcarr.official@gmail.com>
2024-03-07 11:10:09 +0530

    Sam | Made all fields mandatory

--- src/controllers/serviceMaster/serviceMaster.schema.ts
+++ src/controllers/serviceMaster/serviceMaster.schema.ts
@@ -3,9 +3,9 @@ import { z } from "zod";
 import {inputFieldSchema} from "../schemas/zodSchemas.js";
 
 const postServiceMasterSchema = z.object({
-    type: inputFieldSchema.optional(),
-    subtype: inputFieldSchema.optional(),
-    name: inputFieldSchema.optional()
+    type: inputFieldSchema,
+    subtype: inputFieldSchema,
+    name: inputFieldSchema
 });
 
 export default postServiceMasterSchema;
