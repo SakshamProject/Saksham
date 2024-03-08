213533cd809dcc4ec654b889af9384e1309f818b
Samuel Mameru Carr <samuelcarr.official@gmail.com>
2024-03-08 15:23:22 +0530

    Sam | Convert 'true' and 'false' strings to true boolean types

--- src/controllers/schemas/zodSchemas.ts
+++ src/controllers/schemas/zodSchemas.ts
@@ -6,7 +6,7 @@ const getRequestSchema = z.object({
     rows: z.coerce.number().positive().optional(),
     start: z.coerce.number().positive().transform((val) => val - 1).optional(),
     orderBy: z.string().optional(),
-    reverse: z.enum(["true", "false"]).optional()
+    reverse: z.string().toLowerCase().transform((val) => val === "true").optional()
 });
 
 // TODO: Allow Punctuation
