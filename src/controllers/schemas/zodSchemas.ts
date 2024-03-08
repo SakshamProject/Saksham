71a37a72430ce75b7029f7dce7fe0c6600735794
Samuel Mameru Carr <samuelcarr.official@gmail.com>
2024-03-08 15:34:36 +0530

    Sam | Added Helpful comment

--- src/controllers/schemas/zodSchemas.ts
+++ src/controllers/schemas/zodSchemas.ts
@@ -7,6 +7,9 @@ const getRequestSchema = z.object({
     start: z.coerce.number().positive().transform((val) => val - 1).optional(),
     orderBy: z.string().optional(),
     reverse: z.string().toLowerCase().transform((val) => val === "true").optional()
+    // ^^
+    // Since "true" and "false" are both non-empty strings they are converted to true
+    // so instead of z.enum(["true", "false"]) we explicitly convert them to boolean
 });
 
 // TODO: Allow Punctuation
