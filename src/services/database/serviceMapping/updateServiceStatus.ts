e16d5c8e392e8ae6cb137f0c3e5af42d51a4202a
Shashank-Sathiya <shashank@ajirasoft.com>
2024-03-11 09:53:26 +0530

    shashank | update for service mapping

---
+++ src/services/database/serviceMapping/updateServiceStatus.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+import {promise} from "zod";
+
+const prisma = new PrismaClient();
+
+async function updateServiceStatus(): Promise<void> {
+    try {
+        var divyangMobileNo
+
+        // const getDivayangDetails: object | null = await prisma.divyangServiceMapping.findUnique({})
+    }
+    catch (error) {
+        throw error;
+    }
+}
