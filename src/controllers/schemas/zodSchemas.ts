1fa305dcd09605c49ed6f17bb3b37775abe874c7
Shashank-Sathiya <shashank@ajirasoft.com>
2024-03-11 09:50:00 +0530

    shashank | created zod schema

---
+++ src/controllers/schemas/zodSchemas.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+import defaults from "../../defaults.js";
+
+const getRequestSchema = z.object({
+    rows: z.coerce.number().positive().optional(),
+    start: z.coerce.number().positive().transform((val) => val - 1).optional(),
+    orderBy: z.string().optional(),
+    reverse: z.enum(["true", "false"]).optional()
+});
+
+// TODO: Allow Punctuation
+const inputFieldSchema = z.string()
+        .min(defaults.minFieldLength)
+        .trim()
+        .regex(/^[\w\s.-]+$/gm, "No Special Characters. Allowed: [A-Z, a-z, 0-9, ., -, _]");
+
+export { getRequestSchema, inputFieldSchema };
