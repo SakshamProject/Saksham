452742644b4430363b9030d35f5bdb88acedb912
Samuel Mameru Carr <samuelcarr.official@gmail.com>
2024-03-12 16:58:49 +0530

    Sam | Initialize Mocha, Chai and Supertest for testing

---
+++ tests/server.test.ts
@@ -0,0 +1,21 @@
+import supertest from "supertest";
+import {StatusCodes} from "http-status-codes";
+import {expect} from "chai";
+import server from "../server.js";
+
+const testServer = supertest(server);
+describe("# Server", () => {
+    describe("GET /api/check", () => {
+
+        it ("Should check if the API is alive", async () => {
+            try {
+                const response = await testServer.get("/api/check");
+                expect(response.status).to.be.equal(StatusCodes.OK);
+            }
+            catch (error) {
+                console.log(error);
+            }
+        });
+
+    });
+});
