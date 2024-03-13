3a38a5e73b536c1af89c0ae5727122c51e749150
Samuel Mameru Carr <samuelcarr.official@gmail.com>
2024-03-13 12:47:20 +0530

    Sam | Add clearTableDB()

--- src/services/database/database.ts
+++ src/services/database/database.ts
@@ -13,4 +13,18 @@ async function pingDB(): Promise<void> {
     }
 }
 
-export { pingDB };
+// WARN: This function is for testing only!
+// Do not use in code
+async function clearTableDB(tables: string[]): Promise<void> {
+    try {
+        for (const table of tables) {
+            // Delete ALL rows of the database
+            await prisma["user"].deleteMany({ where: {}});
+        }
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
+export { pingDB, clearTableDB };
