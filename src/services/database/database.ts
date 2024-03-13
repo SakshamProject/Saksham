b15c74ca374bf60de6772a4cd95448ed9d52b60c
Samuel Mameru Carr <samuelcarr.official@gmail.com>
2024-03-13 12:48:29 +0530

    Sam | Remove unused Stuff

--- src/services/database/database.ts
+++ src/services/database/database.ts
@@ -1,12 +1,11 @@
 
 import { PrismaClient } from "@prisma/client";
-import {promise} from "zod";
 
 const prisma = new PrismaClient();
 
 async function pingDB(): Promise<void> {
     try {
-        const result: { result: number }[] = await prisma.$queryRaw`SELECT 1 + 1 as result`;
+        await prisma.$queryRaw`SELECT 1 + 1 as result`;
     }
     catch (error) {
         throw error;
