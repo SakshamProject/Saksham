11f1a99692895e3f1cd850d299408406bcc6d12b
Shashank-Sathiya <shashank@ajirasoft.com>
2024-04-01 16:19:45 +0530

    shashank | fixed folder structure

--- src/services/database/typeMaster/generalMaster/educationalQualification/get.ts
+++ src/services/database/typeMaster/generalMaster/educationalQualification/get.ts
@@ -1,6 +1,6 @@
-import { getEducationalQualificationSchema } from "../../../../types/typeMaster/generalMaster/educationalQualificationSchema.js";
-import prisma from "../../database.js"
-import throwDatabaseError from "../../utils/errorHandler.js";
+import { getEducationalQualificationSchema } from "../../../../../types/typeMaster/generalMaster/educationalQualificationSchema.js";
+import prisma from "../../../database.js"
+import throwDatabaseError from "../../../utils/errorHandler.js";
 
 const getEducationalQualificationDB = async (): Promise<getEducationalQualificationSchema | undefined> => {
     try {
