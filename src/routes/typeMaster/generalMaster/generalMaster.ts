251c1019027c54b61433a4c05237bebaa32774e5
Madhumitha Natarajan <madhu@Madhumithas-MacBook-Pro.local>
2024-04-01 18:49:45 +0530

    madhu| added routes for serviceType

--- src/routes/typeMaster/generalMaster/generalMaster.ts
+++ src/routes/typeMaster/generalMaster/generalMaster.ts
@@ -4,7 +4,7 @@ import educationalQualificationRouter from "./educationalQualification.js";
 const generalMasterRouter = express.Router();
 
 generalMasterRouter.use("/educationalQualification", educationalQualificationRouter)
-generalMasterRouter.use()
+generalMasterRouter.use("/serviceType",)
 generalMasterRouter.use()
 
 export default generalMasterRouter;
---
+++ src/routes/typeMaster/generalMaster/serviceType/serviceType.ts
@@ -0,0 +1,14 @@
+import express from "express";
+//import educationalQualificationRouter from "./educationalQualification.js";
+
+const serviceTypeRouter = express.Router();
+
+serviceTypeRouter.get("/",);
+serviceTypeRouter.get("/:id",);
+serviceTypeRouter.post("/",);
+serviceTypeRouter.put("/:id",);
+serviceTypeRouter.delete("/:id",);
+
+
+
+export default serviceTypeRouter;
