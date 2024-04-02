cbf82be0228656d4d2ec0384e07bed4a57831bc7
maign <casamaignanaganapathy@gmail.com>
2024-04-02 12:27:43 +0530

    maign | service layer municipality added

---
+++ src/services/database/typeMaster/stateMaster/municipality/read.ts
@@ -0,0 +1,82 @@
+import { orderByDirectionEnum } from "../../../../../controllers/getRequest.schema.js";
+import defaults from "../../../../../defaults.js";
+import { Municipality } from "../../../../../types/typeMaster/stateMaster/municipalitySchema.js";
+import prisma from "../../../database.js";
+import throwDatabaseError from "../../../utils/errorHandler.js";
+
+const getMunicipalityDB = async (
+  sortOrder: orderByDirectionEnum = orderByDirectionEnum.ascending,
+  start: number = defaults.skip,
+  rows: number = defaults.take,
+  searchText: string
+): Promise<Municipality[] | undefined> => {
+  try {
+    const municipalities: Municipality[] = await prisma.municipality.findMany({
+      where: {
+        name: {
+          contains: searchText,
+        },
+      },
+      orderBy: {
+        name: sortOrder,
+      },
+      skip: start,
+      take: rows,
+    });
+    return municipalities;
+  } catch (error) {
+    if (error instanceof Error) throwDatabaseError(error);
+  }
+};
+
+const getMunicipalityByDistrictIdDB = async (
+  districtId: string,
+  sortOrder: orderByDirectionEnum = orderByDirectionEnum.ascending,
+  start: number = defaults.skip,
+  rows: number = defaults.take
+): Promise<Municipality[] | undefined> => {
+  try {
+    const municipalities: Municipality[] = await prisma.municipality.findMany({
+      where: {
+        districtId: districtId,
+      },
+      orderBy: {
+        name: sortOrder,
+      },
+      skip: start,
+      take: rows,
+    });
+    return municipalities;
+  } catch (error) {
+    if (error instanceof Error) throwDatabaseError(error);
+  }
+};
+
+const getMunicipalityByIdDB = async (
+  id: string,
+  sortOrder: orderByDirectionEnum = orderByDirectionEnum.ascending,
+  start: number = defaults.skip,
+  rows: number = defaults.take
+): Promise<Municipality | undefined | null> => {
+  try {
+    const municipalities: Municipality | null = await prisma.municipality.findFirst(
+      {
+        where: {
+          id: {
+            equals: id,
+          },
+        },
+        orderBy: {
+          name: sortOrder,
+        },
+        skip: start,
+        take: rows,
+      }
+    );
+    return municipalities;
+  } catch (error) {
+    if (error instanceof Error) throwDatabaseError(error);
+  }
+};
+
+export { getMunicipalityDB, getMunicipalityByDistrictIdDB, getMunicipalityByIdDB };
