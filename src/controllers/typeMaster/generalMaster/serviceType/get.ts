83825708e3ef0a6bda4d91b37609e03e5290c0cf
MadhumithaNatarajan21 <madhumithanatarajan22@gmail.com>
2024-04-02 14:14:08 +0530

    madhu| added get request for service Type

--- src/controllers/typeMaster/generalMaster/serviceType/get.ts
+++ src/controllers/typeMaster/generalMaster/serviceType/get.ts
@@ -1,39 +1,49 @@
-import { Request, Response,NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import { getServiceTypeWithServiceSchema } from "../../../../types/typeMaster/generalMaster/serviceTypeSchema.js";
-import { getServiceTypeByIdDB } from "../../../../services/database/typeMaster/generalMaster/serviceType/read.js";
+import { getServiceTypeByIdDB, getServiceTypeCount, getServiceTypeDB } from "../../../../services/database/typeMaster/generalMaster/serviceType/read.js";
 import getRequestSchema from "../../../getRequest.schema.js";
 
-
-async function getServiceTypeById(request:Request, response:Response, next:NextFunction){
-    try{
-        const id = request.params.id;
-        console.log(id)
-        const result:getServiceTypeWithServiceSchema|undefined|null = await getServiceTypeByIdDB(id);
-        response.send(result);
-    }catch(err){
-        
-        next(err);
-    }
+async function getServiceTypeById(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  try {
+    const id = request.params.id;
+    console.log(id);
+    const result: getServiceTypeWithServiceSchema | undefined | null =
+      await getServiceTypeByIdDB(id);
+    response.send(result);
+  } catch (err) {
+    next(err);
+  }
 }
 
-async function getServiceType(request:Request, response:Response, next:NextFunction){
-    const query = getRequestSchema.parse(
-        request.query
-      );
+async function getServiceType(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const query = getRequestSchema.parse(request.query);
 
-      //const orderByColumn: string | undefined = orderBy;
+    const results = await getServiceTypeDB(
+      query.start,
+      query.rows,
+      query.orderByColumn,
+      query.orderByDirection,
+      query.searchText,
+    );
 
-      console.log(query);
-    
-    //   const results = await getDesignationDB(
-    //     start,
-    //     rows,
-    //     orderByColumn,
-    //     orderByDirection
-    //   );
-    
-    //   response.send(results);
+    const count:number  = await getServiceTypeCount();
 
+    response.send({
+        result:results,
+        count:count,
+        start:query.start,
+        rows:query.rows,
+        orderByColumn:query.orderByColumn,
+        orderByDirection:query.orderByDirection
+    });
 }
 
-export {getServiceTypeById,getServiceType};
+export { getServiceTypeById, getServiceType };
---
+++ src/controllers/typeMaster/generalMaster/serviceType/put.ts
@@ -0,0 +1,7 @@
+import { NextFunction, Request, Response } from "express";
+
+async function putServiceType(request:Request,response:Response,next:NextFunction){
+
+    
+
+}
