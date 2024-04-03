780bd36fc4739e1c0a8f63e6afb4ed1666639a06
Samuel Mameru Carr <samuelcarr.official@gmail.com>
2024-04-04 00:00:11 +0530

    Sam | Add P1001 Error

--- src/services/database/utils/errorHandler.ts
+++ src/services/database/utils/errorHandler.ts
@@ -2,8 +2,18 @@ import {Prisma} from "@prisma/client";
 import APIError from "../../errors/APIError.js";
 import {StatusCodes} from "http-status-codes";
 
+const DatabaseConnectivityError = "P1001";
+
 function throwDatabaseError(error: Error) {
     if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === DatabaseConnectivityError) {
+            throw new APIError(
+                "Could not connect to the database",
+                StatusCodes.INTERNAL_SERVER_ERROR,
+                "DatabaseConnectivityError",
+                "S"
+            );
+        }
         if (error.code === "P2025") {
             throw new APIError(
                 "The specified record could not be found",
