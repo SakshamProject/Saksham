f213d89e10e90d575c17d7781fd3057afb0ce7d1
MadhumithaNatarajan21 <madhumithanatarajan22@gmail.com>
2024-04-04 13:08:13 +0530

    madhu | added a route to get generalmasterseed

--- src/routes/typeMaster/generalMaster/generalMaster.ts
+++ src/routes/typeMaster/generalMaster/generalMaster.ts
@@ -1,13 +1,25 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import educationalQualificationRouter from "./educationalQualification.js";
 import serviceTypeRouter from "./serviceType.js";
 import disabilityTypeRouter from "./disabilityType.js";
+import prisma from "../../../services/database/database.js";
+import { createResponseOnlyData } from "../../../types/createResponseSchema.js";
 
 const generalMasterRouter = express.Router();
 
-generalMasterRouter.use("/education-qualification", educationalQualificationRouter)
-generalMasterRouter.use("/servicetype",serviceTypeRouter)
-generalMasterRouter.use("/disabilitytype",disabilityTypeRouter)
-
+generalMasterRouter.use(
+  "/education-qualification",
+  educationalQualificationRouter
+);
+generalMasterRouter.use("/servicetype", serviceTypeRouter);
+generalMasterRouter.use("/disabilitytype", disabilityTypeRouter);
+generalMasterRouter.get(
+  "/generalmasterseed",
+  async (request: Request, response: Response, next: NextFunction) => {
+    const generalMaster = await prisma.generalMasters.findMany();
+    const responseData = createResponseOnlyData(generalMaster || {});
+    response.send(responseData);
+  }
+);
 
 export default generalMasterRouter;
