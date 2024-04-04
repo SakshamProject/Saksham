1d599ec1c702abca8e81c2eb547aeeb26555bffb
MadhumithaNatarajan21 <madhumithanatarajan22@gmail.com>
2024-04-04 17:14:41 +0530

    madhu | commit before merge

--- src/routes/typeMaster/generalMaster/generalMaster.ts
+++ src/routes/typeMaster/generalMaster/generalMaster.ts
@@ -1,12 +1,9 @@
 import express from "express";
-import educationalQualificationRouter from "./educationalQualification.js";
-import serviceTypeRouter from "./serviceType.js";
+
 import { communityCategoryRouter } from "./communityCategory.js";
 
 const generalMasterRouter = express.Router();
 
-generalMasterRouter.use("/education-qualification", educationalQualificationRouter)
-generalMasterRouter.use("/servicetype",serviceTypeRouter)
 generalMasterRouter.use("/communitycategory", communityCategoryRouter)
 
 export default generalMasterRouter;
--- src/routes/typeMaster/typeMaster.ts
+++ src/routes/typeMaster/typeMaster.ts
@@ -1,10 +1,9 @@
 import express from "express";
-import stateMasterRouter from "./stateMaster/stateMaster.js";
 import generalMasterRouter from "./generalMaster/generalMaster.js";
 
 const typeMasterRouter = express.Router();
 
-typeMasterRouter.use("/statemaster", stateMasterRouter);
+
 typeMasterRouter.use("/generalmaster", generalMasterRouter);
 
 
