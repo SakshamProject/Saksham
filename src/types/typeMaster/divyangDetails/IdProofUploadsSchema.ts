91887e2e779a96511805ab0a1b5ef2e394bc6808
Shashank-Sathiya <shashank@ajirasoft.com>
2024-04-05 12:45:06 +0530

    shashank | IdProofUploads zod schema for divyang details created

---
+++ src/types/typeMaster/divyangDetails/IdProofUploadsSchema.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod'
+import inputFieldSchema, { uuidSchema } from '../../inputFieldSchema.js'
+
+const IdProofUploadsRequestSchema = z.object({
+  voterId: inputFieldSchema,
+  panCardNumber: z.string().regex(/^[A-Z]{5}[0-9]{4}[A-Z]$/, "Invalid PAN card number format"),
+  drivingLicense: z.string().length(64),
+  rationCardNumber: z.string().length(64),
+  aadharCardNumber: z.string().length(12).regex(/^\d{12}$/, "Invalid Aadhaar number format"),
+  pensionCardNumber: z.string().length(64),
+  medicalInsuranceNumber: z.string().length(64),
+  disabilitySchemeNumber: z.string().length(64),
+  BPL_OR_APL_Number: z.string().length(64),
+})
