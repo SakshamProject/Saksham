0c0af3c5507a504020d595ca4e69dd82c804d17d
Shashank-Sathiya <shashank@ajirasoft.com>
2024-04-05 13:36:43 +0530

    shashank | address schema incomplete

---
+++ src/types/typeMaster/divyangDetails/addressSchema.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+import inputFieldSchema, { uuidSchema } from "../../inputFieldSchema.js";
+
+const addressRequestSchema = z.object({
+    doorNumber: inputFieldSchema,
+    flatNumber: inputFieldSchema,
+    streetName: inputFieldSchema,
+    nagarName: inputFieldSchema,
+    districtId: uuidSchema,
+    isRural: z.boolean(),
+    villageName: inputFieldSchema,
+    panchayatUnionId: uuidSchema,
+    talukId: uuidSchema,
+    townPanchayatId: uuidSchema,
+    municipalityId: uuidSchema,
+    corporationId: uuidSchema,
+    MLAConstituencyId: uuidSchema,
+    MPConstituancyId: uuidSchema,
+    pincode: z.number()
+})
