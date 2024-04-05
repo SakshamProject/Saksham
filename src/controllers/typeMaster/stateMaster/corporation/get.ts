719ec09735d0067f27c7fd96d2d4b810f74da4a9
maign <casamaignanaganapathy@gmail.com>
2024-04-05 18:33:14 +0530

    maign | changed getbydistrictId response format

--- src/controllers/typeMaster/stateMaster/corporation/get.ts
+++ src/controllers/typeMaster/stateMaster/corporation/get.ts
@@ -74,7 +74,7 @@ const getCorporationByDistrictId = async (
     const count: number = result?.corporation?.length || 0;
     const total: number = result?.total || 0;
     const responseData = createResponseWithQuery(
-      result || {},
+      result?.corporation || {},
       query,
       total,
       count
