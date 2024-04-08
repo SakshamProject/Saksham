e3d6224b9be7302b44da542ee02556726c0dfb05
Shashank-Sathiya <shashank@ajirasoft.com>
2024-04-08 12:39:51 +0530

    shashank | divyang detials zod schema

--- src/types/divyangDetails/divyangDetailsSchema.ts
+++ src/types/divyangDetails/divyangDetailsSchema.ts
@@ -1,14 +1,20 @@
-import { z } from "zod";
-import { personalDetailsRequestSchema } from "./personalDetailsSchema.js";
-import { IdProofUploadsRequestSchema } from "./IdProofUploadsSchema.js";
-import { addressRequestSchema } from "./addressSchema.js";
-import { disabiltyDetailsRequestSchema } from "./disabilityDetailsSchema.js";
-import { employmentDetailsRequestSchema } from "./employmentDetailsSchema.js";
+import { z } from 'zod'
+import { personalDetailsRequestSchema } from './personalDetailsSchema.js'
+import { IdProofUploadsRequestSchema } from './IdProofUploadsSchema.js'
+import { addressRequestSchema } from './addressSchema.js'
+import { disabiltyDetailsRequestSchema } from './disabilityDetailsSchema.js'
+import { employmentDetailsRequestSchema } from './employmentDetailsSchema.js'
 
-const divyangDetailsRequestSchema = z.object({
-    personalDetailsRequestSchema: personalDetailsRequestSchema,
-    IdProofUploadsRequestSchema: IdProofUploadsRequestSchema,
-    addressRequestSchema: addressRequestSchema,
-    disabiltyDetailsRequestSchema: disabiltyDetailsRequestSchema,
-    employmentDetailsRequestSchema: employmentDetailsRequestSchema
-}).refine()
+const divyangDetailsRequestSchema = z
+  .object({
+    personalDetails: personalDetailsRequestSchema,
+    IdProofUploads: IdProofUploadsRequestSchema,
+    addressRequest: addressRequestSchema,
+    disabiltyDetails: disabiltyDetailsRequestSchema,
+    employmentDetails: employmentDetailsRequestSchema,
+  })
+  .refine((data) => {
+    return Object.values(data).some((value) => value !== undefined)
+  }, 'At least one of the five schemas must be provided.')
+
+export type DivyangDetailsRequest = z.infer<typeof divyangDetailsRequestSchema>
