bd8aa3b231e3fb622d92c67ef5e2e465e84f8dd9
mameru-carr <samuelcarr.official@gmail.com>
2024-04-08 16:38:16 +0530

    Sam | Remove unnecessary database functions

--- src/services/database/database.ts
+++ src/services/database/database.ts
@@ -1,6 +1,5 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import throwDatabaseError from "./utils/errorHandler.js";
-import searchTextMapper from "./utils/serviceMaster/searchTextMapper.js";
 
 const prisma = new PrismaClient();
 
@@ -16,47 +15,5 @@ async function pingDB(): Promise<void> {
 }
 
 
-// WARN: This function is for testing only!
-// Do not use in code
-async function clearTableDB(tables: Prisma.ModelName): Promise<void> {
-    try {
-        for (const table of tables) {
-            // Delete ALL rows of the database
-            // await prisma[table].deleteMany({ where: {}});
-        }
-    }
-    catch(error) {
-        if (error instanceof Error) {
-            throwDatabaseError(error);
-          }
-    }
-}
-
-async function getTotalRowsDB(table: Prisma.ModelName) {
-    try {
-        const rows = await prisma[table].count();
-        return rows;
-    }
-    catch(error) {
-        if (error instanceof Error) {
-            throwDatabaseError(error);
-        }
-    }
-}
-
-async function getTotalRowsWithOrWithoutFilterDB(table: Prisma.ModelName, searchText: string = "") {
-    try {
-        const count = await prisma[table].count({
-            where: searchTextMapper(table, searchText)
-        });
-
-        return count;
-    }
-    catch(error) {
-        if (error instanceof Error) {
-            throwDatabaseError(error);
-        }
-    }
-}
 export default prisma;
-export { pingDB, clearTableDB, getTotalRowsDB, getTotalRowsWithOrWithoutFilterDB };
+export { pingDB };
