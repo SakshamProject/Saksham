645e0801da275617bcaa7644fc0ffd32d7099423
mameru-carr <samuelcarr.official@gmail.com>
2024-04-09 11:24:59 +0530

    Sam | Remove unused import

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -1,13 +1,11 @@
 import supertest from "supertest";
 import {StatusCodes} from "http-status-codes";
-import config from "../config.js";
 import { expect } from "chai";
 import server from "../server.js";
 
 const testServer = supertest(server);
 describe("# Server", () => {
     describe("GET /api/check", () => {
-
         it ("Should check if the API is alive", async () => {
             const response = await testServer.get("/check");
             expect(response.status).to.be.equal(StatusCodes.OK);
