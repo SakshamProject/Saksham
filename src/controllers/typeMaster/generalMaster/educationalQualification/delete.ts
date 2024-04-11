efdf3f0f3abab10865a3d8509529f59ac6e725c8
Shashank-Sathiya <shashank@ajirasoft.com>
2024-04-11 16:42:07 +0530

    shashank | fixed bug in put API education qualification

--- src/controllers/typeMaster/generalMaster/educationalQualification/delete.ts
+++ src/controllers/typeMaster/generalMaster/educationalQualification/delete.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Response, Request } from 'express'
 import { deleteEducationQualificationTypeDB } from '../../../../services/database/typeMaster/generalMaster/educationalQualification/delete.js'
 import { createResponseOnlyData } from '../../../../types/createResponseSchema.js'
+import prisma from '../../../../services/database/database.js'
 
 async function deleteEducationQualificationType(
   request: Request,
@@ -10,7 +11,8 @@ async function deleteEducationQualificationType(
   try {
     const id: string = request.params.id
     const deletedEducationQualification = await deleteEducationQualificationTypeDB(
-      id,
+      prisma,
+      id
     )
     const responseData = createResponseOnlyData(deletedEducationQualification || {})
     response.send(responseData)
