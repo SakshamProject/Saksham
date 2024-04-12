579ffa9dfce231d4b3d57003ee2a580ee0b082ea
mameru-carr <samuelcarr.official@gmail.com>
2024-04-12 12:48:13 +0530

    Sam | Made fields mandatory and fixed type issue

--- src/types/users/usersSchema.ts
+++ src/types/users/usersSchema.ts
@@ -5,24 +5,23 @@ import {specialCharsRegex} from "../regex.js";
 
 const usersPostSchema = z.object({
     sevaKendraId: uuidSchema.optional(),
-    userId: z.string().optional(),
-    firstName: z.string().optional(),
-    lastName: z.string().optional(),
+    userId: z.string(),
+    firstName: z.string(),
+    lastName: z.string(),
     gender: z.nativeEnum(GenderEnum).optional(),
     dateOfBirth: dateSchema.optional(),
-    designationId: z.string().uuid().optional(),
-    mail: emailSchema.optional(),
-    contactNumber: phoneNumberSchema.optional(),
-    whatsappNumber: phoneNumberSchema.optional(),
-    loginId: phoneNumberSchema.optional(),
-    password: z.string().regex(specialCharsRegex).optional(),
+    designationId: z.string().uuid(),
+    email: emailSchema,
+    contactNumber: phoneNumberSchema,
+    whatsappNumber: phoneNumberSchema,
+    loginId: phoneNumberSchema,
+    password: z.string().regex(specialCharsRegex),
     // audit log
-    status: z.nativeEnum(AuditLogStatusEnum).optional(),
-    effectiveDate: dateSchema.optional(), // effective date
-    // description: inputFieldSchema
+    status: z.nativeEnum(AuditLogStatusEnum),
+    effectiveDate: dateSchema, // effective date
     description: z.string().optional()
 });
-const userPostRequestType = z.infer<typeof usersPostSchema>;
+type userPostRequestType = z.infer<typeof usersPostSchema>;
 
 const usersPutSchema = z.object({
     sevaKendraId: uuidSchema,
@@ -40,10 +39,9 @@ const usersPutSchema = z.object({
     // audit log
     status: z.nativeEnum(AuditLogStatusEnum),
     effectiveDate: dateSchema,
-    description: z.string()
-    // description: inputFieldSchema
+    description: inputFieldSchema
 });
-const userPutRequestType = z.infer<typeof usersPutSchema>;
+type userPutRequestType = z.infer<typeof usersPutSchema>;
 
 export { usersPostSchema, usersPutSchema};
 export { userPostRequestType, userPutRequestType };
