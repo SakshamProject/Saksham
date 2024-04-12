4deca91a0369070cf0b4885f7ec1767baed9150e
mameru-carr <samuelcarr.official@gmail.com>
2024-04-12 12:48:23 +0530

    Sam | Create dto

--- src/dto/users/post.ts
+++ src/dto/users/post.ts
@@ -1,8 +1,34 @@
 import { Prisma } from "@prisma/client";
 import { userPostRequestType } from "../../types/users/usersSchema.js";
 
-function createUserDBObject(body: userPostRequestType): Prisma.UserUncheckedCreateInput {
+function createUserDBObject(body: userPostRequestType): Prisma.UserCreateInput {
     const userInputObject: Prisma.UserCreateInput = {
+        userId: body.userId,
+        firstName: body.firstName,
+        lastName: body.lastName,
+        gender: body.gender,
+        dateOfBirth: body.dateOfBirth,
+        contactNumber: body.contactNumber,
+        whatsappNumber: body.whatsappNumber,
+        mail: body.email,
+        loginid: body.loginId,
+        designation: {
+            connect: {
+                id: body.designationId
+            }
+        },
+        password: {
+            create: {
+                hashedPassword: "abcd@123"
+            }
+        },
+        userAuditLog: {
+            create: {
+                description: body.description,
+                status: body.status,
+                date: body.effectiveDate,
+            }
+        }
     }
     return userInputObject;
 }
