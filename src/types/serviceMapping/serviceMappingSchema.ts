e4fa22d49073b3bec68b6017e25817d97b6bbe9e
MadhumithaNatarajan21 <madhumithanatarajan22@gmail.com>
2024-04-12 13:02:41 +0530

    madhu | completed schema for post service mapping

--- src/types/serviceMapping/serviceMappingSchema.ts
+++ src/types/serviceMapping/serviceMappingSchema.ts
@@ -1,24 +1,31 @@
 import { z } from "zod";
-import { dateSchema, uuidSchema } from "../inputFieldSchema.js";
+import inputFieldSchema, { dateSchema, emailSchema, phoneNumberSchema, uuidSchema } from "../inputFieldSchema.js";
+
+const nonSevaKendraFollowUpSchema = z.object({
+name:inputFieldSchema,
+mobile:phoneNumberSchema,
+email:emailSchema,
+sendEmail:z.boolean()
+})
 
 const postServiceMappingRequestSchema = z.object({
-//     id              
-//   divyangId          
-//   userId             
-//   serviceId          
-                              
-//   dateOfService      
-//   dueDate            
-
-//   isCompleted  
+ 
 divyangId : uuidSchema,     
 sevaKendraId:uuidSchema,
 userId:uuidSchema,
 serviceId:uuidSchema,
 dateOfService:dateSchema,
 dueDate:dateSchema,
-
-
-
-  
-})
+isNonSevaKendraVolunteerRequired:z.boolean(),
+nonSevaKendraFollowUp:nonSevaKendraFollowUpSchema
+ 
+}).refine((data)=>{
+    if(data.sevaKendraId){
+        return data.userId!==undefined&&data.nonSevaKendraFollowUp==undefined
+    }else{
+        return data.sevaKendraId!==undefined&&data.userId!==undefined&&data.nonSevaKendraFollowUp==undefined
+    }
+}).transform(data => ({
+    ...data,
+    isNonSevaKendraVolunteerRequired: data.sevaKendraId ? false : true
+  }));
