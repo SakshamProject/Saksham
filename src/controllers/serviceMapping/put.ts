634ac26f2dfcd000652fbfc74c41c0ddd60f1269
MadhumithaNatarajan21 <madhumithanatarajan22@gmail.com>
2024-04-15 15:50:38 +0530

    madhu | modified followUp sevakendra schema

---
+++ src/controllers/serviceMapping/put.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+
+async function putServiceMapping(request:Request,response:Response,next:NextFunction){
+    try{
+
+        const body = ;
+
+    }catch(err){
+        next(err);
+    }
+
+}
