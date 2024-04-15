f86d43e775918986db05c7eb21a4ede035ee79b3
maign <casamaignanaganapathy@gmail.com>
2024-04-15 10:17:14 +0530

    maign | added service mapping schema and schema type

--- src/types/serviceMapping/serviceMappingScreens.ts
+++ src/types/serviceMapping/serviceMappingScreens.ts
@@ -2,6 +2,7 @@ import { Prisma } from "@prisma/client";
 import { z } from "zod";
 import { filterOperationsEnum } from "../inputFieldSchema.js";
 import { ServiceMappingColumnNamesEnum } from "./serviceMappingDefaults.js";
+import { sortOrderEnum } from "../getRequestSchema.js";
 
 type ServiceMappingWhere = Prisma.DivyangServiceMappingWhereInput;
 
@@ -13,6 +14,28 @@ const serviceMappingFilter = z
   })
   .array();
 type serviceMappingFilterType = z.infer<typeof serviceMappingFilter>;
-const getServiceMappingSchema = z.object({});
-
-export { ServiceMappingWhere };
+const getServiceMappingSchema = z.object({
+  filter: serviceMappingFilter.optional(),
+  pagination: z
+    .object({
+      rows: z.number().positive(),
+      start: z
+        .number()
+        .positive()
+        .transform((number) => number - 1),
+    })
+    .optional(),
+  searchText: z.string().optional(),
+  sorting: z
+    .object({
+      orderByColumn: z.nativeEnum(ServiceMappingColumnNamesEnum),
+      sortOrder: z.nativeEnum(sortOrderEnum),
+    })
+    .optional(),
+});
+type getServiceMappingSchemaType = z.infer<typeof getServiceMappingSchema>;
+export {
+  ServiceMappingWhere,
+  getServiceMappingSchema,
+  getServiceMappingSchemaType,
+};
