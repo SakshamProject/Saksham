7f9b1a929278d6dc1846ac4ad89bc7150fcefe27
Shashank-Sathiya <shashank@ajirasoft.com>
2024-04-16 14:47:55 +0530

    shashank | added file handling to divyang details

--- src/routes/divyangDetails/divyangDetails.ts
+++ src/routes/divyangDetails/divyangDetails.ts
@@ -6,13 +6,35 @@ import {
 import { postDivyangDetails } from '../../controllers/divyangDetails/post.js'
 import { deleteDivyangDetails } from '../../controllers/divyangDetails/delete.js'
 import { putDivyangDetails } from '../../controllers/divyangDetails/put.js'
+import fileHandler from '../../middlewares/fileHandler/fileHandler.js'
 
 const divyangDetailsRouter = express.Router()
 
 divyangDetailsRouter.post('/list/', getDivyangDetails)
 divyangDetailsRouter.get('/:id', getDivyangDetailsbyId)
-divyangDetailsRouter.post('/', postDivyangDetails)
-divyangDetailsRouter.put('/:id', putDivyangDetails)
+divyangDetailsRouter.post(
+  '/',
+  fileHandler.single('picture'),
+  postDivyangDetails,
+)
+divyangDetailsRouter.put(
+  '/:id',
+  fileHandler.fields([
+    { name: 'picture', maxCount: 1 },
+    { name: 'voterId', maxCount: 1 },
+    { name: 'panCard', maxCount: 1 },
+    { name: 'drivingLicense', maxCount: 1 },
+    { name: 'rationCard', maxCount: 1 },
+    { name: 'aadharCard', maxCount: 1 },
+    { name: 'pensionCard', maxCount: 1 },
+    { name: 'medicalInsuranceCard', maxCount: 1 },
+    { name: 'disabilitySchemeNumber', maxCount: 1 },
+    { name: 'BPL_OR_APL_Number', maxCount: 1 },
+    { name: 'disabilityCard', maxCount: 1 },
+    { name: 'UDIDCard', maxCount: 1 }
+  ]),
+  putDivyangDetails,
+)
 // divyangDetailsRouter.delete('/:id', deleteDivyangDetails)
 
 export { divyangDetailsRouter }
