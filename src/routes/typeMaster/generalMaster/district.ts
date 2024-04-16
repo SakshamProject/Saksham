0714d8cd328625c82bc78fe0eecd47132e85c283
maign <casamaignanaganapathy@gmail.com>
2024-04-16 12:50:38 +0530

    maign | changed get by districtId function

--- src/routes/typeMaster/generalMaster/district.ts
+++ src/routes/typeMaster/generalMaster/district.ts
@@ -6,6 +6,7 @@ import {
 } from "../../../controllers/typeMaster/generalMaster/district/get.js";
 import { deleteDistrict } from "../../../controllers/typeMaster/generalMaster/district/delete.js";
 import { updateDistrict } from "../../../controllers/typeMaster/generalMaster/district/update.js";
+import { getSevaKendraByDistrictId } from "../../../controllers/sevaKendra/get.js";
 
 const districtRouter = express.Router();
 districtRouter.post("/", postDistrict);
@@ -13,5 +14,5 @@ districtRouter.get("/", getDistrict);
 districtRouter.get("/:id", getDistrictById);
 districtRouter.delete("/:id", deleteDistrict);
 districtRouter.put("/:id", updateDistrict);
-
+districtRouter.get("/:id/sevakendras", getSevaKendraByDistrictId);
 export default districtRouter;
