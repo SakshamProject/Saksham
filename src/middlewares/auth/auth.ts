23150f76b64501a507186c047c4ad03a906fc4ca
mameru-carr <samuelcarr.official@gmail.com>
2024-04-18 11:31:07 +0530

    Sam | Removed logs

--- src/middlewares/auth/auth.ts
+++ src/middlewares/auth/auth.ts
@@ -1,12 +1,10 @@
 import { NextFunction, Request, Response } from "express";
-import log from "../../services/logger/logger.js";
 
 function authenticate(request: Request, response: Response, next: NextFunction) {
     const user: User = {
         id: "d62ea177-8fef-44d5-8776-807a8399a8db", // User Id
     };
     request.user = user;
-    // log("info", "[middleware/auth]:\n %o", user);
     next();
 }
 
