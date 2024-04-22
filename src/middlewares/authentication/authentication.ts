62297e8c729271194359b8ea6cde50e90cd2002d
=sanjay-asoft <=sanjay@ajirasoft.com>
2024-04-22 16:19:12 +0530

    sanjay,mai|finished put and post users

--- src/middlewares/authentication/authentication.ts
+++ src/middlewares/authentication/authentication.ts
@@ -5,7 +5,7 @@ import log from "../../services/logger/logger.js";
 
 function authenticate(request: Request, response: Response, next: NextFunction) {
     const user:User = {
-      id: "30a3e3de-0242-4998-95f5-79158100bc31",
+      id: "f4e1ab7c-f5e8-46f6-afc5-02063030f62d",
     };
     log("info", "[middleware/auth] user: %o", user);
     request.user = user;
