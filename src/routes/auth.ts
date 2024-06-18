655248e902800d5730c0bf3fe9519f173fdf7083
maign <casamaignanaganapathy@gmail.com>
2024-06-18 11:47:05 +0530

    Maign | auth file handling change

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,32 +1,32 @@
-import express, { NextFunction, Request, Response, Router } from "express";
-import userLogin from "../controllers/authentication/userLogIn.js";
-import userLogout from "../controllers/authentication/userLogout.js";
-import divyangLogin from "../controllers/authentication/divyangLogin.js";
-import divyangLogout from "../controllers/authentication/divyangLogout.js";
-import { postDivyang } from "../controllers/divyangDetails/post.js";
-import divyangForgetPassword from "../controllers/authentication/divyangForgotPassword.js";
-import updatePassword from "../controllers/authentication/updatePassword.js";
-import userForgetPassword from "../controllers/authentication/userForgotPassword.js";
-import errorHandler from "../middlewares/errorHandler/errorHandler.js";
-import superAdminLogin from "../controllers/authentication/superAdmin.js";
-import fileHandler from "../middlewares/fileHandler/fileHandler.js";
+import express, { Router } from 'express';
+import userLogin from '../controllers/authentication/userLogIn.js';
+import userLogout from '../controllers/authentication/userLogout.js';
+import divyangLogin from '../controllers/authentication/divyangLogin.js';
+import divyangLogout from '../controllers/authentication/divyangLogout.js';
+import { postDivyang } from '../controllers/divyangDetails/post.js';
+import divyangForgetPassword from '../controllers/authentication/divyangForgotPassword.js';
+import updatePassword from '../controllers/authentication/updatePassword.js';
+import userForgetPassword from '../controllers/authentication/userForgotPassword.js';
+import errorHandler from '../middlewares/errorHandler/errorHandler.js';
+import superAdminLogin from '../controllers/authentication/superAdmin.js';
+import fileHandler from '../middlewares/fileHandler/fileHandler.js';
 const authRouter = Router();
 
 authRouter.use(express.json());
 
-authRouter.post("/divyang/login", divyangLogin);
+authRouter.post('/divyang/login', divyangLogin);
 authRouter.post(
-  "/divyang/signup",
-  fileHandler.single("profilePhoto"),
+  '/divyang/signup',
+  fileHandler.fields([{ name: 'profilePhoto', maxCount: 1 }]),
   postDivyang
 );
-authRouter.post("/divyang/logout", divyangLogout);
-authRouter.post("/divyang/forgetpassword", divyangForgetPassword);
-authRouter.post("/user/login", userLogin);
-authRouter.post("/user/logout", userLogout);
-authRouter.post("/user/forgetpassword", userForgetPassword);
-authRouter.post("/updatePassword/:id", updatePassword);
-authRouter.post("/admin/login", superAdminLogin);
+authRouter.post('/divyang/logout', divyangLogout);
+authRouter.post('/divyang/forgetpassword', divyangForgetPassword);
+authRouter.post('/user/login', userLogin);
+authRouter.post('/user/logout', userLogout);
+authRouter.post('/user/forgetpassword', userForgetPassword);
+authRouter.post('/updatePassword/:id', updatePassword);
+authRouter.post('/admin/login', superAdminLogin);
 
 authRouter.use(errorHandler);
 
