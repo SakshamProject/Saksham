5042760d557508f724ea093612015dec6870c59e
maign <casamaignanaganapathy@gmail.com>
2024-06-19 12:05:32 +0530

    maign | added dto for disabilityOfdivyang

---
+++ src/dto/divyangDetails/disabilityOfDivyang/post.ts
@@ -0,0 +1,37 @@
+import { Prisma } from '@prisma/client';
+import { DisabilityOfDivyang } from '../../../types/divyangDetails/disabilityDetailsSchema.js';
+
+const createDisabilityOfDivyangDBObject = (
+  disability: DisabilityOfDivyang,
+  key: string
+) => {
+  const newDisability: Prisma.DisabilityOfDivyangCreateInput = {
+    disabilityType: {
+      connect: { id: disability.disabilityTypeId },
+    },
+    divyang: {
+      connect: {
+        id: disability.divyangId,
+      },
+    },
+    certificateIssueAuthority: disability.certificateIssueAuthority,
+    dateOfIssue: disability.dateOfIssue,
+    disabilityArea: disability.disabilityArea,
+    disabilityDueTo: disability.disabilityDueTo,
+    disabilityPercentage: disability.disabilityPercentage,
+    disabilitySince: disability.disabilitySince,
+    isDisabilitySinceBirth: disability.isDisabilitySinceBirth,
+  };
+  if (key) {
+    newDisability.disabilityCardKey = key;
+    newDisability.disabilityCardFileName = disability.disabilityCardFileName;
+  }
+  if (disability.disabilitySubTypeId) {
+    newDisability.disabilitySubType = {
+      connect: { id: disability.disabilitySubTypeId },
+    };
+  }
+  return newDisability;
+};
+
+export { createDisabilityOfDivyangDBObject };
