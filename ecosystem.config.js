4acfa049da42ff3f99117804e1aca0851d62081f
maign <casamaignanaganapathy@gmail.com>
2024-07-15 12:38:58 +0530

    Maign | changed build files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'saksham-backend',
-      script: './build/index.js',
+      script: './dist/server.js',
       interpreter: '/home/ubuntu/.nvm/versions/node/v16.20.1/bin/node',
       node_args: '--max-http-header-size=81000 --tls-min-v1.0',
       exec_mode: 'cluster',
